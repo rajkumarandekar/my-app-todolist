8aef9ecc85580debd9d55bde088a053e24cbb9c2
rajkumarandekar <rajkumarandekar1144@gmail.com>
2023-05-26 19:41:02 +0530

    n

--- src/UserList.js
+++ src/UserList.js
@@ -6,15 +6,34 @@ const UserList = () => {
   const [todos, setTodos] = useState([]);
   const [newText, setNewText] = useState("");
   const [editTodoId, setEditTodoId] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    fetchTodos();
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+      setIsLoggedIn(true);
+      fetchTodos();
+    }
   }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [isLoggedIn, token]);
 
   const fetchTodos = async () => {
     try {
       const response = await axios.get(
-        "https://bakcend-todo.onrender.com/api/todos"
+        "https://bakcend-todo.onrender.com/api/todos",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
       setTodos(response.data);
     } catch (error) {
@@ -24,9 +43,15 @@ const UserList = () => {
 
   const handleAddTodo = async () => {
     try {
-      await axios.post("https://bakcend-todo.onrender.com/api/todos", {
-        text: newText,
-      });
+      await axios.post(
+        "https://bakcend-todo.onrender.com/api/todos",
+        { text: newText },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       fetchTodos(); // Refresh the todo list
       setNewText("");
     } catch (error) {
@@ -38,7 +63,13 @@ const UserList = () => {
     try {
       const response = await axios.put(
         `https://bakcend-todo.onrender.com/api/todos/${id}`,
-        { text: newText }
+
+        { text: newText },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
       console.log(response.data); // Optional: Log the updated todo item
       fetchTodos(); // Refresh the todo list
@@ -50,7 +81,11 @@ const UserList = () => {
 
   const handleDeleteTodo = async (id) => {
     try {
-      await axios.delete(`https://bakcend-todo.onrender.com/api/todos/${id}`);
+      await axios.delete(`https://bakcend-todo.onrender.com/api/todos/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       fetchTodos(); // Refresh the todo list
     } catch (error) {
       console.error(error);
@@ -66,62 +101,92 @@ const UserList = () => {
     const todoToUpdate = todos.find((todo) => todo._id === id);
     setNewText(todoToUpdate.text);
   };
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/login", {
+        username: "",
+        password: "",
+      });
+      setToken(response.data);
+      setIsLoggedIn(true);
+      fetchTodos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setToken("");
+    setTodos([]);
+  };
 
   return (
     <div className="user-list-container">
       <h1 className="user-list-title">Todo List</h1>
-      <div className="add-todo-container">
-        <input
-          type="text"
-          value={newText}
-          onChange={handleEditInputChange}
-          disabled={editTodoId !== null}
-          className="todo-input"
-        />
-        <button className="add-todo-button" onClick={handleAddTodo}>
-          Add Todo
-        </button>
-      </div>
-      {todos.length === 0 ? (
-        <p>No todos found</p>
+      {isLoggedIn ? (
+        <>
+          <div className="add-todo-container">
+            <input
+              type="text"
+              value={newText}
+              onChange={handleEditInputChange}
+              disabled={editTodoId !== null}
+              className="todo-input"
+            />
+            <button className="add-todo-button" onClick={handleAddTodo}>
+              Add Todo
+            </button>
+          </div>
+          {todos.length === 0 ? (
+            <p>No todos found</p>
+          ) : (
+            <ul>
+              {todos.map((todo) => (
+                <li key={todo._id}>
+                  {editTodoId === todo._id ? (
+                    <>
+                      <input
+                        type="text"
+                        value={newText}
+                        onChange={handleEditInputChange}
+                      />
+                      <button
+                        className="save-todo-button"
+                        onClick={() => handleEditTodo(todo._id)}
+                      >
+                        Save
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {todo.text}
+                      <button
+                        className="edit-todo-button"
+                        onClick={() => handleEditButtonClick(todo._id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="delete-todo-button"
+                        onClick={() => handleDeleteTodo(todo._id)}
+                      >
+                        Delete
+                      </button>
+                    </>
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
+          <button className="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
+        </>
       ) : (
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo._id}>
-              {editTodoId === todo._id ? (
-                <>
-                  <input
-                    type="text"
-                    value={newText}
-                    onChange={handleEditInputChange}
-                  />
-                  <button
-                    className="save-todo-button"
-                    onClick={() => handleEditTodo(todo._id)}
-                  >
-                    Save
-                  </button>
-                </>
-              ) : (
-                <>
-                  {todo.text}
-                  <button
-                    className="edit-todo-button"
-                    onClick={() => handleEditButtonClick(todo._id)}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="delete-todo-button"
-                    onClick={() => handleDeleteTodo(todo._id)}
-                  >
-                    Delete
-                  </button>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
+        <button className="login-button" onClick={handleLogin}>
+          Login
+        </button>
       )}
     </div>
   );
